faeb859b41a1f831ec0f90c436323f51146c6324
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2022-09-08 00:12:18 +0300

    настраиваю первоначальную сборку

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+import gulp from 'gulp';
+import browser from 'browser-sync';
+import plumber from 'gulp-plumber';
+import sass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+
+// Styles
+
+export const styles = () => {
+  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(browser.stream());
+}
+
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/*.html').on('change', browser.reload);
+}
+
+// Server 
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+export default gulp.series(
+  styles, server, watcher
+);
