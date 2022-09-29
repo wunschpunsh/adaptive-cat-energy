672cb0aef448163a782f2e14398e053cd3e00e18
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2022-09-29 17:51:41 +0300

    fgf

--- docs/js/main.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.querySelector('.main-header__button');
-const naviBar = document.querySelector('.main-header__navigation')
-menuButton.addEventListener('click', () => {
-  naviBar.classList.toggle('main-nav--opened')
-})
