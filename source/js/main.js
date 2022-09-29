01a4451ab0f100456bf9f3aba521e9e370e34a12
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2022-09-29 17:44:44 +0300

    адаптивная верстка крупных блоков главной страницы

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 const menuButton = document.querySelector('.main-header__button');
 const naviBar = document.querySelector('.main-header__navigation')
 menuButton.addEventListener('click', () => {
-  naviBar.classList.toggle('main-nav--closed')
+  naviBar.classList.toggle('main-nav--opened')
 })
