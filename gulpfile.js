294bd8e9538b7af7fdacb00329fb3aa85ef24cbe
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2022-10-07 19:32:33 +0300

    реализован стайлгайд

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,40 @@ import autoprefixer from 'autoprefixer';
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+  return gulp
+    .src('source/sass/style.scss', { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
-}
-
+};
 
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
-}
+  gulp.watch('source/js/**/*.js').on('change', browser.reload);
+};
+// const watcher = () => {
+//   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+//   gulp.watch('source/js/main.js', gulp.series(scripts, reload));
+//   gulp.watch('source/*.html', gulp.series(html, reload));
+// }
 
-// Server 
+// Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'source',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
-export default gulp.series(
-  styles, server, watcher
-);
+export default gulp.series(gulp.parallel(styles), gulp.series(server, watcher));
